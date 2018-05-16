6a91e37832d60260f69fcfbc38f2859454b96322
User <golikov.d.s@yandex.ru>
2018-05-16 13:51:50 +0300

    Initial commit

---
+++ src/main/java/ru/artsok/ArtsokErpApplication.java
@@ -0,0 +1,12 @@
+package ru.artsok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtsokErpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtsokErpApplication.class, args);
+	}
+}
